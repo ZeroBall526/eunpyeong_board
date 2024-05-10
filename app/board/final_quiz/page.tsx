3dc1fd9cc8893aa9b25b4f78a9bfc01366490038
USER <bumseocbc11@gmail.com>
2024-05-11 04:32:17 +0900

    feat:add final quiz

--- app/board/final_quiz/page.tsx
+++ app/board/final_quiz/page.tsx
@@ -1,7 +1,94 @@
 export default function Final_quiz() {
     return(
         <main>
-            fina_quiz zone
+            <section className="mx-[100px] font-Pretendard font-bold">
+                <h1 className="text-5xl py-10">퀴즈를 통하여📝<br></br>나도 컴퓨터공학 마스터</h1>
+
+                <div className="bg-slate-50 text-black text-3xl rounded-lg dark:bg-gray-800 dark:text-gray-400">
+                    <div className="m-10 py-10">
+                        <a>1. 컴퓨터공학이란 다양한 형태의 정보에 대해 기술을
+                        <br></br>통합적으로 가지고 노는 비 첨단 학문이다.</a>
+                        <div className="pt-10 float: inline-start;">
+                        <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                            <input id="q1-1" type="radio" value="" name="q1" className="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
+                            <label for="q1-1" class="w-full py-4 ms-2">⭕</label>
+                            </div>
+                            <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                            <input id="q1-2" type="radio" value="" name="q1" className="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
+                            <label for="q1-2" class="w-full py-4 ms-2">❌</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="bg-slate-50 text-black text-3xl rounded-lg dark:bg-gray-800 dark:text-gray-400">
+                    <div className="m-10 py-10">
+                        <a>2. 객체지향이란 하나의 정보나 상태를 여러객체로 나누어 
+                        표현함을 의미한다.
+                        </a>
+                        <div className="pt-10 float: inline-start;">
+                        <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                            <input id="q2-1" type="radio" value="" name="q2" className="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
+                            <label for="q2-1" class="w-full py-4 ms-2">⭕</label>
+                            </div>
+                            <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                            <input id="q2-2" type="radio" value="" name="q2" className="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
+                            <label for="q2-2" class="w-full py-4 ms-2">❌</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="bg-slate-50 text-black text-3xl rounded-lg dark:bg-gray-800 dark:text-gray-400">
+                    <div className="m-10 py-10">
+                        <a>3. 사용자에게 보여주는 페이지를 만드는 개발자를
+                        ‘Backend’라 부른다.
+                        </a>
+                        <div className="pt-10 float: inline-start;">
+                        <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                            <input id="q2-1" type="radio" value="" name="q2" className="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
+                            <label for="q2-1" class="w-full py-4 ms-2">⭕</label>
+                            </div>
+                            <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                            <input id="q2-2" type="radio" value="" name="q2" className="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
+                            <label for="q2-2" class="w-full py-4 ms-2">❌</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="bg-slate-50 text-black text-3xl rounded-lg dark:bg-gray-800 dark:text-gray-400">
+                    <div className="m-10 py-10">
+                        <a>4. 한국 아이티 5대공룡을 ‘네카라쿠바’라 줄여 말한다.
+                        </a>
+                        <div className="pt-10 float: inline-start;">
+                        <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                            <input id="q2-1" type="radio" value="" name="q2" className="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
+                            <label for="q2-1" class="w-full py-4 ms-2">⭕</label>
+                            </div>
+                            <div className="flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
+                            <input id="q2-2" type="radio" value="" name="q2" className="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
+                            <label for="q2-2" class="w-full py-4 ms-2">❌</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <button className="bg-slate-50 text-2xl text-black px-6 py-3 rounded-lg dark:focus:ring-gray-700 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">제출하기!</button>
+            </section>
+            <footer  className="font-Pretenard justify-center">
+                <div className="font-medium text-center">
+                Copyright 2024. 가람메. All rights reserved.
+                </div>
+            </footer>    
         </main>
     );
 } 
